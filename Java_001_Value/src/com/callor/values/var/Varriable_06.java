fa4acd236b8990c29a789121bbf8cc69fea5f073
soyeon-noh <1010qwerty12@gmail.com>
2021-03-17 16:42:16 +0900

    변수연습

---
+++ Java_001_Value/src/com/callor/values/var/Varriable_06.java
@@ -0,0 +1,34 @@
+package com.callor.values.var;
+
+public class Varriable_06 {
+
+	public static void main(String[] args) {
+		
+		// 정수형 변수선언과 clear
+		int num1 = 0;
+		Integer num2 = 0;
+		
+		// int, Integer로 선언된 변수는 정수값만 
+		// 저장할 수 있다.
+		// 최대 -2^31 ~ 2^(31-1) 범위까지 저장 가능
+		
+		num1 = 30;
+		num2 = 30;
+		
+		// -2^63 ~ 2^(63-1) 범위까지 저장 가능
+		long numLong1 = 30L;
+		Long numLong2 = 30L;
+		
+		
+		// 실수형 변수선언과 clear
+		float num3 = 30.0F; // 소수점 7자리 까지
+		Float num4 = 30.0F;
+		
+		double num5 = 30.0; // float의 2배의 정밀도
+		Double num6 = 30.0;
+		
+		
+		
+		
+	}
+}
