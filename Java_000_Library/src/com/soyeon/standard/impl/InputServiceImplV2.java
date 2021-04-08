94e8e6c24d7b3017a7b180142095c2374750ec7f
soyeon-noh <1010qwerty12@gmail.com>
2021-04-08 11:32:33 +0900

    나만의 JDK 만들기

---
+++ Java_000_Library/src/com/soyeon/standard/impl/InputServiceImplV2.java
@@ -0,0 +1,25 @@
+package com.soyeon.standard.impl;
+
+import com.soyeon.standard.InputService;
+
+public class InputServiceImplV2 implements InputService {
+
+	@Override
+	public Integer inputValue(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer inputVlaue(String title, int start) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer inputVlaue(String title, int start, int end) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
