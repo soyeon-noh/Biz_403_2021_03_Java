792e1ec3cc22e7aa11a46576cb30a4470ebad8f5
soyeon-noh <1010qewrty12@gmail.com>
2021-04-20 15:40:30 +0900

    WordServiceImplV2

---
+++ Java_020_WordQuiz/src/com/callor/word/WordEx_01.java
@@ -0,0 +1,12 @@
+package com.callor.word;
+
+import com.callor.word.service.WordService;
+import com.callor.word.service.impl.WordServiceImplV2;
+
+public class WordEx_01 {
+
+	public static void main(String[] args) {
+		WordService wService = new WordServiceImplV2();
+			wService.startGame();
+	}
+}
